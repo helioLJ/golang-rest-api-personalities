83942427bc15531895414078cf018c7d759029f5
HelioLucio <helio.lucio.jr@hotmail.com>
2024-03-18 08:14:11 -0300

    Add Content-Type header to TodasPersonalidades endpoint

--- routes/routes.go
+++ routes/routes.go
@@ -6,10 +6,12 @@ import (
 
 	"github.com/gorilla/mux"
 	"github.com/helioLJ/golang-rest-api-personalities/controllers"
+	"github.com/helioLJ/golang-rest-api-personalities/middleware"
 )
 
 func HandleRequest() {
 	r := mux.NewRouter()
+	r.Use(middleware.ContentTypeMiddleware)
 	r.HandleFunc("/", controllers.Home)
 	r.HandleFunc("/api/personalidades", controllers.TodasPersonalidades).Methods("Get")
 	r.HandleFunc("/api/personalidades/{id}", controllers.RetornaUmaPersonalidade).Methods("Get")
